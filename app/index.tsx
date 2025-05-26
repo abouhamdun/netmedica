16c80c72545cce01218049968616545ce9769fc2
abouhumaida <adambabangida14@gmail.com>
2025-05-27 00:43:40 +0100

    creating auth screens

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+export default function welcomeScreen() {
+  return (
+    <View className='text-2xl'>
+      <Text>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas, fuga doloribus praesentium, cumque enim accusantium temporibus eos possimus incidunt nihil nulla, animi quo totam adipisci dignissimos mollitia. Non, modi vel!</Text>
+    </View>
+  )
+}
